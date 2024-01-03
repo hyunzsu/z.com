5a9f6aae948575b070120c1905806cb23b8c44d8
hyunzsu <kkm5169@naver.com>
2024-01-03 11:59:22 +0900

    Style | PostForm 생성

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,14 +1,15 @@
+import PostForm from './_component/PostForm';
 import Tab from './_component/Tab';
 import TabProvider from './_component/TabProvider';
 import styles from './home.module.css';
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <TabProvider>
         <Tab />
-        {/* <PostForm />
-        <Post />
+        <PostForm />
+        {/* <Post />
         <Post />
         <Post />
         <Post />
