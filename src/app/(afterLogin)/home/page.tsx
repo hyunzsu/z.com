e80b76bad6fa0d6c1057195675ef418ad900e3a1
hyunzsu <kkm5169@naver.com>
2024-01-03 11:27:42 +0900

    Feat | 홈탭 만들면서 Context API 적용

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,26 @@
+import Tab from './_component/Tab';
+import TabProvider from './_component/TabProvider';
+import styles from './home.module.css';
+
 export default function Home() {
-  return <main>홈페이지</main>;
+  return (
+    <main>
+      <TabProvider>
+        <Tab />
+        {/* <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post /> */}
+      </TabProvider>
+    </main>
+  );
 }
