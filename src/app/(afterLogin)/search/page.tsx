af67447907b368761e7cf3fb5f92ce85c94f6425
hyunzsu <hyunzsu@naver.com>
2024-01-07 19:55:50 +0900

    Feat | /search 페이지에 useSearchParams 사용

--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -1,5 +1,41 @@
-// import styles from './page.module.css';
+import BackButton from '@/app/(afterLogin)/_component/BackButton';
+import SearchForm from '@/app/(afterLogin)/_component/SearchForm';
+import Tab from '@/app/(afterLogin)/search/_component/Tab';
+import Post from '@/app/(afterLogin)/_component/Post';
+import styles from './search.module.css';
 
-export default function Home() {
-  return <main>검색페이지</main>;
+type Props = {
+  searchParams: { q: string; f?: string; pf?: string };
+};
+
+export default function Search({ searchParams }: Props) {
+  return (
+    <main className={styles.main}>
+      <div className={styles.searchTop}>
+        <div className={styles.searchZone}>
+          <div className={styles.buttonZone}>
+            <BackButton />
+          </div>
+          <div className={styles.formZone}>
+            <SearchForm q={searchParams.q} />
+          </div>
+        </div>
+        <Tab />
+      </div>
+      <div className={styles.list}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        {/*<SearchResult searchParams={searchParams} />*/}
+      </div>
+    </main>
+  );
 }
